915706c50f3f82d64c22f5e32f4fc62528eba2a9
elegarmi <elegarmi@gmail.com>
2022-02-16 23:47:50 +0100

    Added optional new styles

--- src/components/Home/Product/Product.jsx
+++ src/components/Home/Product/Product.jsx
@@ -5,9 +5,19 @@ const Product = (props) => {
   return (
     <div className="card">
       <img className="card__img" src={image} alt={name} />
-      <h3 className="card__title"> {name}</h3>
-      <p className="card__info card__price">{`Precio ${price}`}</p>
-      <p className="card__info card__details">Ver detalles</p>
+
+      <div className="card__body">
+        <h3 className="card__title">{name}</h3>
+        <p className="card__info card__price">{`Precio ${price}`}</p>
+      </div>
+
+      <div className="card__buttons">
+        <a href="/" className="card__info card__details card__button">Ver detalles</a>
+      </div>
+
+      <div className="card__footer">
+        <p className="card__info card__advertiser">Anunciante</p>
+      </div>
     </div>
   );
 };
