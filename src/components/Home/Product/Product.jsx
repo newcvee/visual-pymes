73123763bfef70727c86d85f9a9815d225cd2f4a
elegarmi <elegarmi@gmail.com>
2022-02-17 00:05:02 +0100

    Added card styles

--- src/components/Home/Product/Product.jsx
+++ src/components/Home/Product/Product.jsx
@@ -7,17 +7,18 @@ const Product = (props) => {
       <img className="card__img" src={image} alt={name} />
 
       <div className="card__body">
-        <h3 className="card__title">{name}</h3>
-        <p className="card__info card__price">{`Precio ${price}`}</p>
+        <div className="card__info">
+          <h3 className="card__title">{name}</h3>
+          <p className="card__price">{`Precio ${price}`}</p>
+        </div>
+        <div className="card__buttons">
+          <a href="/" className="card__info card__details card__button">Ver detalles</a>
+        </div>
       </div>
-
-      <div className="card__buttons">
-        <a href="/" className="card__info card__details card__button">Ver detalles</a>
-      </div>
-
       <div className="card__footer">
         <p className="card__info card__advertiser">Anunciante</p>
       </div>
+
     </div>
   );
 };
